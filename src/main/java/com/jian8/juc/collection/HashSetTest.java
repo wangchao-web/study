989266a7707c25f7f30963b6fa9ffe57407dbe68
wangchao_jkcpx <wangchao_jkcpx@kedacom.com>
2020-11-27 17:08:53 +0800

    study

---
+++ src/main/java/com/jian8/juc/collection/HashSetTest.java
@@ -0,0 +1,57 @@
+package com.jian8.juc.collection;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * hashSet线程不安全
+ */
+public class HashSetTest {
+    public static void main(String[] args) {
+//        notSafe();
+//        safe1();
+        safe2();
+    }
+    /**
+     * 故障现象
+     * java.util.ConcurrentModificationException
+     */
+    public static void notSafe() {
+        Set<String> list = new HashSet<>();
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, "Thread " + i).start();
+        }
+    }
+
+    /**
+     * 使用Collections辅助类
+     * synchronizedSet
+     */
+    public static void safe1() {
+        Set<String> list = Collections.synchronizedSet(new HashSet<>());
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, "Thread " + i).start();
+        }
+    }
+
+    /**
+     * 优化解决方案3
+     * CopyOnWriteArraySet
+     * 写实复制，读写分离的思想
+     */
+    public static void safe2() {
+        Set<String> list = new CopyOnWriteArraySet<>();
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, "Thread " + i).start();
+        }
+    }
+}
